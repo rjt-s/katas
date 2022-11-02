d2036e8344823a891a48689a6b5d839167f90edb
Rajat Sharma <srma.rjt@gmail.com>
2022-11-02 00:13:12 -0400

    kata 1

--- intro.js
+++ intro.js
@@ -1,1 +1,27 @@
 // LHL Katas 
+
+const sumLargestNumbers = function(data) {
+  if(data.length < 2){
+    return null;
+  }
+  let largestNum = data[0];
+  let largestIndex = 0;
+  let secondLargestNum = data[0];
+  for(let i = 0; i < data.length; i++){
+    if(data[i] > largestNum){
+      largestNum = data[i];
+      largestIndex = i;
+    }
+  }
+  data.splice(largestIndex,1);
+  for(let i = 0; i < data.length; i++){
+    if(data[i] > secondLargestNum){
+      secondLargestNum = data[i];
+    }
+  }
+  return largestNum + secondLargestNum;
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
